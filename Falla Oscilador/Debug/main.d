7aa145060648889de5a0ad43bc407dd8d1306203
alfonsoredfi <38919119+alfonsoredfi@users.noreply.github.com>
2018-10-24 23:26:14 -0500

    Add files via upload

---
+++ Falla Oscilador/Debug/main.d
@@ -0,0 +1,13 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs720/ccsv7/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccs720/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs720/ccsv7/tools/compiler/ti-cgt-msp430_17.6.0.STS/include/intrinsics.h
+main.obj: C:/ti/ccs720/ccsv7/tools/compiler/ti-cgt-msp430_17.6.0.STS/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+C:/ti/ccs720/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccs720/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs720/ccsv7/tools/compiler/ti-cgt-msp430_17.6.0.STS/include/intrinsics.h: 
+C:/ti/ccs720/ccsv7/tools/compiler/ti-cgt-msp430_17.6.0.STS/include/intrinsics_legacy_undefs.h: 
